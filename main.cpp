888275c8bf0d267205ab6f07ea3a69f63b996a6a
tjanjua04 <71242006+tjanjua04@users.noreply.github.com>
2023-10-29 08:07:38 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<memory>
+#include<vector>
+#include<string>
+// Write template class Tab here
+// Write template class Browser here
+//Add display method in Browser template class
+/*void display(){
+auto curr = head;
+std::cout<<"Browser tab list = "<<std::endl;
+while(curr){
+std::cout<<"| "<<curr->name<<" x|-->";
+curr = curr->next;
+}
+std::cout<<std::endl;
+std::cout<<std::endl;
+}
+*/
+int main(){
+/*Browser<double> b1;
+b1.addNewTab("https://www.google.com","Google",23.45);
+b1.display();
+std::cout<<"Switch to previous tab = "<<std::endl;
+b1.switchToPrevTab();
+std::cout<<"Switch to Next tab = "<<std::endl;
+b1.switchToNextTab();
+b1.addNewTab("https://www.youtube.com","YouTube",56);
+b1.bookmarkCurrent();
+b1.display();
+b1.addNewTab("https://www.geeksforgeeks.com","GeeksForGeeks",45.78);
+b1.bookmarkCurrent();
+b1.addNewTab("https://chat.openai.com","ChatGPT",129);
+b1.addNewTab("https://linkedin.com","LinkedIn",410);
+b1.bookmarkCurrent();
+b1.addNewTab("https://github.com","Github",110);
+b1.addNewTab("https://kaggle.com","Kaggle",310);
+b1.bookmarkCurrent();
+b1.display();
+std::cout<<"Total memory consumption = "<<b1.total_memory()<<"MB"<<std::endl;
+b1.showBookmarkTab();
+b1.moveCurrentToFirst();
+b1.display();
+b1.deleteTab();
+b1.display();
+std::cout<<"Switch to next tab = "<<std::endl;
+b1.switchToNextTab();
+std::cout<<"Switch to previous tab = "<<std::endl;
+b1.switchToPrevTab();
+b1.closeCurrentTab();
+b1.display();
+std::cout<<"Switch to previous tab = "<<std::endl;
+b1.switchToPrevTab();
+b1.closeCurrentTab();
+b1.display();
+b1.showBookmarkTab();
+std::cout<<"Total Memory Consumption = "<<b1.total_memory()<<"MB"<<std::endl;
+b1.deleteTab();
+b1.display();
+b1.addNewTab("https://docs.google.com/","Google Docs",102.34);
+b1.display();
+std::cout<<"Switch to previous tab = "<<std::endl;
+b1.switchToPrevTab();
+std::cout<<"Switch to previous tab = "<<std::endl;
+b1.switchToPrevTab();
+std::cout<<"Switch to previous tab = "<<std::endl;
+b1.switchToPrevTab();
+b1.bookmarkCurrent();
+b1.showBookmarkTab();
+b1.total_memory();
+b1.deleteTab();
+b1.display();*/
+return 0;
+}
