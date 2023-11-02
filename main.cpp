5655aa4a3caa4ecf5153474205094242858f9743
tjanjua04 <71242006+tjanjua04@users.noreply.github.com>
2023-11-02 12:35:53 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,21 +3,76 @@
 #include<vector>
 #include<string>
 // Write template class Tab here
+template<typename T> class Tab {
+  public:
+    std::string url;
+    std::string Name;
+    T memory;
+    Tab* next;
+    Tab* prev;
+};
 // Write template class Browser here
-//Add display method in Browser template class
-/*void display(){
-auto curr = head;
-std::cout<<"Browser tab list = "<<std::endl;
-while(curr){
-std::cout<<"| "<<curr->name<<" x|-->";
-curr = curr->next;
-}
-std::cout<<std::endl;
-std::cout<<std::endl;
-}
-*/
+template<typename T> class Browser{
+  public:
+    void addNewTab(){
+      std::string <Tab<string>> string url;
+      std::string <Tab<string>> string name;
+      std::string <Tab<T>> T memory;
+      // create new tab
+      // assign values to tab values and add it to the last position of the linked list
+      // insert tab to the end and make it the current browser
+    }
+    void switchToPrevTab(){
+      // switch from current tab to previous tab
+      // print details of the tab
+      // if there is no prev tab say "no previous tab"
+    }
+    void switchToNextTab(){
+      // switch from current tab to next tab
+      // print details of the tab
+      // if there is no next tan print "no next tab"
+    } 
+    void closeCurrentTab(){
+      // delete current tab
+      // current tab becomes next tab
+      // print "new current tab is = "
+    }
+    void bookmarkCurrent(){
+      // saves url and name to a vector
+      std::vector<std::pair<std::string>> bookmark
+    }
+    void showBookmarkTab(){
+      // print bookmark
+      // print name and url
+      // basically just print the vector
+    }
+    void moveCurrentToFirst(){
+      // move current browser to the first position in the linked list
+    } 
+    void total_memory(){
+      // print total memory
+    }
+    void deleteTab(){
+      // delete whichever tab has the highest memory
+      // print which tab was deleted
+    }
+
+    //Add display method in Browser template class
+    void display(){
+    auto curr = head;
+    std::cout<<"Browser tab list = "<<std::endl;
+    while(curr){
+    std::cout<<"| "<<curr->name<<" x|-->";
+    curr = curr->next;
+    }
+    std::cout<<std::endl;
+    std::cout<<std::endl;
+    }
+
+};
+
 int main(){
-/*Browser<double> b1;
+Browser<double> b1;
 b1.addNewTab("https://www.google.com","Google",23.45);
 b1.display();
 std::cout<<"Switch to previous tab = "<<std::endl;
@@ -68,6 +123,6 @@ b1.bookmarkCurrent();
 b1.showBookmarkTab();
 b1.total_memory();
 b1.deleteTab();
-b1.display();*/
+b1.display();
 return 0;
 }
